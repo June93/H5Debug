93932ca2e3f1d49be06f053627505380fc306f1e
pengj02 <pengj02@mysoft.com.cn>
2016-06-29 10:48:23 +0800

    首发消息

--- ios/H5Debug/H5Debug/AppDelegate.m
+++ ios/H5Debug/H5Debug/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "EMSDK.h"
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,23 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    EMOptions *options = [EMOptions optionsWithAppkey:@"acetrump#h5-debug"];
+    
+    [[EMClient sharedClient] initializeSDKWithOptions:options];
+    
+    EMError *error = [[EMClient sharedClient] loginWithUsername:@"iphone" password:@"123456"];
+    if (!error) {
+        
+        NSLog(@"登录成功");
+        
+        [[EMClient sharedClient].options setIsAutoLogin:YES];
+        
+    } else {
+        
+        NSLog(@"%@", error.errorDescription);
+    }
+    
     return YES;
 }
 
@@ -26,12 +45,15 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    // APP进入后台
+    [[EMClient sharedClient] applicationDidEnterBackground:application];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
+    // APP进入前台
+    [[EMClient sharedClient] applicationWillEnterForeground:application];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
--- ios/H5Debug/H5Debug/ViewController.m
+++ ios/H5Debug/H5Debug/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "EMClient.h"
+#import "EMMessageBody.h"
+#import "EMTextMessageBody.h"
+#import "EMConversation.h"
+
+@interface ViewController ()<EMChatManagerDelegate>
 
 @end
 
@@ -17,11 +22,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //注册消息回调
+    [[EMClient sharedClient].chatManager addDelegate:self delegateQueue:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btn_sendMsg:(id)sender
+{
+    NSArray *arr_ems = [[EMClient sharedClient].chatManager getAllConversations];
+    if (arr_ems.count == 0) {
+        return;
+    }
+    
+    EMConversation *ems = [arr_ems objectAtIndex:0];
+    
+    EMTextMessageBody *body = [[EMTextMessageBody alloc] initWithText:@"from iphone message"];
+    NSString *from = [[EMClient sharedClient] currentUsername];
+    
+    EMMessage *message = [[EMMessage alloc] initWithConversationID:ems.conversationId from:from to:ems.conversationId body:body ext:nil];
+    message.chatType = EMChatTypeChat; // 设置为单聊消息
+    
+    [[[EMClient sharedClient] chatManager] asyncSendMessage:message progress:^(int progress) {
+        
+        //
+        
+    } completion:^(EMMessage *message, EMError *error) {
+        
+        if (!error) {
+            
+            NSLog(@"消息发送成功");
+        }
+        
+    }];
+}
+
+#pragma mark - EMChatManagerDelegate
+-(void)didReceiveMessages:(NSArray *)aMessages
+{
+    for (EMMessage *message in aMessages) {
+        
+        EMMessageBody *msgBody = message.body;
+        
+        switch (msgBody.type) {
+                
+            case EMMessageBodyTypeText:
+            {
+                //收到的文字消息
+                EMTextMessageBody *textBody = (EMTextMessageBody *)msgBody;
+                NSString *txt = textBody.text;
+                NSLog(@"收到的文字是 txt -- %@",txt);
+                
+                _lblMsg.text = txt;
+            }
+                
+                break;
+                
+            default:
+                break;
+        }
+    }
 }
 
 @end
